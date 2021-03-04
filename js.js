2a8652d7e7acb94b1da9e1f1b3f43f04c8f674b9
Timur <zairovne@gmail.com>
2021-03-04 18:55:02 +0500

    date

---
+++ js.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let hi = document.querySelector('.hi'),
+    today = document.querySelector('.today'),
+    time = document.querySelector('.time');
+    let newYear = document.querySelector('.newyear');
+
+let date = new Date();
+
+//Привествие на странице
+function greeting(){
+    let hours = date.getHours();
+        if(hours > 5 && hours < 11){
+                console.log('Доброе утро');
+        }else if(hours > 11 && hours < 17){
+                console.log('Добрый день');
+        }else if(hours > 17 && hours < 22){
+                console.log('Добрый вечер');
+        }else if(hours > 22 && hours < 5){
+                console.log('Доброй ночи');
+    }
+}
+greeting();
+hi.textContent = ``;
+//день недели
+let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let weekDay = date.getDate();
+today.textContent = `Сегодня: ${days[weekDay]}`;
+
+
+// часы на сегодня
+function timeDate (){
+    return new Date().toTimeString().replace(/ .*/, '');
+}
+setInterval(() => {
+    time.textContent = `Текущее время: ${timeDate()}`
+}, 1000);
+
+//до нового года осталось
+ function NewYear (greatHolyday){
+
+    let dateStop = new Date(greatHolyday).getTime(),
+    timeRamaining = (dateStop - date) / 1000;
+    let day = Math.floor(timeRamaining / 60 /60 / 24);
+    return day;    
+}
+let tillNewYear = NewYear('31 december 2021');
+newYear.textContent =`До нового года осталось ${tillNewYear} дней`;
